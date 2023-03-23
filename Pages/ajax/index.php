2e5539f80f4e2357bddaeb902d82fd09d9151d21
Jarvis <13g7895123>
2023-03-23 20:26:29 +0800

    test notify 03232026

--- Pages/ajax/index.php
+++ Pages/ajax/index.php
@@ -64,13 +64,13 @@ function line_notify($token, $form_data){
         'message' => $msg
     );
     $ch = curl_init();
-    // curl_setopt($ch , CURLOPT_URL , "https://notify-api.line.me/api/notify");
-    // curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    // curl_setopt($ch, CURLOPT_POST, true);
-    // curl_setopt($ch, CURLOPT_POSTFIELDS, $message);
-    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    // $result = curl_exec($ch);
-    // curl_close($ch);
+    curl_setopt($ch , CURLOPT_URL , "https://notify-api.line.me/api/notify");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $message);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
 }
 
 ?>
