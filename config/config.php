4517ea2df072d320909f359732898b8a3d46818a
Jarvis <13g7895123>
2023-03-23 14:40:58 +0800

    test 03231440

--- config/config.php
+++ config/config.php
@@ -2,10 +2,10 @@
 
 // ※連線資料庫設定
 const SQLPORT_SQL = '3306'; //資料庫port
-const HOSTNAME_SQL = '139.162.15.125'; //資料庫位址
-// const HOSTNAME_SQL = 'localhost'; //資料庫位址
-const USERNAME_SQL = 'bonus_loan_remote'; //帳號
-// const USERNAME_SQL = 'bonus_loan_user'; //帳號
+// const HOSTNAME_SQL = '139.162.15.125'; //資料庫位址
+const HOSTNAME_SQL = 'localhost'; //資料庫位址
+// const USERNAME_SQL = 'bonus_loan_remote'; //帳號
+const USERNAME_SQL = 'bonus_loan_user'; //帳號
 const PASSWORD_SQL = '820820'; //密碼
 const DATABASE_SQL = 'db_bonus_loan'; //資料庫名稱
 const SESSION_TIMEOUT = 86400; //session保留時間(單位秒)
