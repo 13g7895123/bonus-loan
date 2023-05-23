b6f808ef5e5bdbf4b28a36add41e0f5931b409d7
Jarvis <13g7895123>
2023-05-23 22:41:38 +0800

    change pic name

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage:{
-        'bg_pic': 'url("../asset/img/bg_big.png")',
+        'bg_pic': 'url("../asset/img/bg_pig.png")',
         'pic2': 'url("https://images.pexels.com/photos/1496372/pexels-photo-1496372.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")'
       }
     },
