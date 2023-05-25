612ff3cebe35e2dd065a41dbe6b17497c1e8a0d4
Jarvis <13g7895123>
2023-05-25 22:32:53 +0800

    05252232

--- index.php
+++ index.php
@@ -24,13 +24,8 @@
     <link rel="stylesheet" href="./dist/output.css" > 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
     
-    <style>
-        body::-webkit-scrollbar {
-            display: none;
-        }
-    </style>
 </head>
-<body class='bg-fixed relative overflow-y-auto overflow-x-hidden'>
+<body class='bg-fixed relative overflow-y-scroll overflow-x-hidden'>
 <!-- <body class='bg-bg_pic bg-fixed relative overflow-y-scroll overflow-x-hidden'> -->
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KW67D7R"
@@ -124,54 +119,95 @@
             <div class='title w-full flex justify-center mb-10'>成功案例</div>
             <div class='w-[95%] flex flex-wrap mx-auto'>
                 <!-- 1 -->
-                <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
+                <div class='w-[50%] md:w-[33.3%] sm:w-full mix-[320px]:w-full flex flex-col items-center my-[40px]'>
                     <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
                         <img src='./asset/img/businessman.png' class='w-[100px] h-auto'>
                     </div>
                     <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>台北張先生<span class='ml-1 text-sm text-[#4d4d4d] font-semibold mb-10'>創業老闆</span></div>
-                    <!-- <p class='md:w-[70%] sm:w-[60%] inf2_p'>初創業需要一份週轉金，五金百貨批貨需要先墊貨款，每常這樣墊也吃不消，有了易速貸，讓他的資金可以先支付代墊，終於不用這麼有壓力</p> -->
-                    <p class='inf2_p'>初創業需要一份週轉金，五金百貨批貨需要先墊貨款，每常這樣墊也吃不消，有了易速貸，讓他的資金可以先支付代墊，終於不用這麼有壓力</p>
+                    <p class='md:w-[80%] sm:w-[60%] min-[320px]:w-[60%] inf2_p'>初創業需要一份週轉金，五金百貨批貨需要先墊貨款，每常這樣墊也吃不消，有了易速貸，讓他的資金可以先支付代墊，終於不用這麼有壓力</p>
                 </div>
+                <!-- <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
+                    <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
+                        <img src='./asset/img/businessman.png' class='w-[100px] h-auto'>
+                    </div>
+                    <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>台北張先生<span class='ml-1 text-sm text-[#4d4d4d] font-semibold mb-10'>創業老闆</span></div>
+                    <p class='inf2_p'>初創業需要一份週轉金，五金百貨批貨需要先墊貨款，每常這樣墊也吃不消，有了易速貸，讓他的資金可以先支付代墊，終於不用這麼有壓力</p>
+                </div> -->
                 <!-- 2 -->
-                <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
+                <div class='w-[50%] md:w-[33.3%] sm:w-full mix-[320px]:w-full flex flex-col items-center my-[40px]'>
+                    <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
+                        <img src='./asset/img/woman.png' class='w-[100px] h-auto'>
+                    </div>
+                    <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>台南李小姐<span class='ml-1 text-sm text-[#4d4d4d] font-semibold mb-10'>美容師</span></div>
+                    <p class='md:w-[80%] sm:w-[60%] min-[320px]:w-[60%] inf2_p'>家人生病需要負擔醫藥費，剛出社會身上也沒有多餘的存款，不想讓家人擔心，很開心遇到易速貸，專業幫忙撥款，解決了臨時的資金需求</p>
+                </div>
+                <!-- <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
                     <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
                         <img src='./asset/img/woman.png' class='w-[100px] h-auto'>
                     </div>
                     <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>台南李小姐<span class='ml-1 text-sm text-[#4d4d4d] font-semibold mb-10'>美容師</span></div>
                     <p class='md:w-[70%] sm:w-[60%] inf2_p'>家人生病需要負擔醫藥費，剛出社會身上也沒有多餘的存款，不想讓家人擔心，很開心遇到易速貸，專業幫忙撥款，解決了臨時的資金需求</p>
-                </div>
+                </div> -->
                 <!-- 3 -->
-                <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
+                <div class='w-[50%] md:w-[33.3%] sm:w-full mix-[320px]:w-full flex flex-col items-center my-[40px]'>
+                    <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
+                        <img src='./asset/img/dad.png' class='w-[100px] h-auto'>
+                    </div>
+                    <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>台中林先生<span class='ml-1 text-sm text-[#4d4d4d] font-semibold mb-10'>旅遊業</span></div>
+                    <p class='md:w-[80%] sm:w-[60%] min-[320px]:w-[60%] inf2_p'>因疫情影響生意，都快支付不了員工的薪資，無意間發現易速貸，沒想到他們這麼專業，可以依需求幫我規劃，讓我可以成功撥款，太感謝了！</p>
+                </div>
+                <!-- <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
                     <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
                         <img src='./asset/img/dad.png' class='w-[100px] h-auto'>
                     </div>
                     <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>台中林先生<span class='ml-1 text-sm text-[#4d4d4d] font-semibold mb-10'>旅遊業</span></div>
                     <p class='md:w-[70%] sm:w-[60%] inf2_p'>因疫情影響生意，都快支付不了員工的薪資，無意間發現易速貸，沒想到他們這麼專業，可以依需求幫我規劃，讓我可以成功撥款，太感謝了！</p>
-                </div>
+                </div> -->
                 <!-- 4 -->
-                <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
+                <div class='w-[50%] md:w-[33.3%] sm:w-full mix-[320px]:w-full flex flex-col items-center my-[40px]'>
+                    <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
+                        <img src='./asset/img/businesswoman.png' class='w-[100px] h-auto'>
+                    </div>
+                    <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>屏東賴小姐<span class='ml-1 text-sm text-[#4d4d4d] font-semibold mb-10'>早餐店店長</span></div>
+                    <p class='md:w-[80%] sm:w-[60%] min-[320px]:w-[60%] inf2_p'>最近因為想跟朋友再投資一間分店，需要預付房租訂金，感謝易速貸，讓我這麼快可以拿到資金</p>
+                </div>
+                <!-- <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
                     <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
                         <img src='./asset/img/businesswoman.png' class='w-[100px] h-auto'>
                     </div>
                     <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>屏東賴小姐<span class='ml-1 text-sm text-[#4d4d4d] font-semibold mb-10'>早餐店店長</span></div>
                     <p class='md:w-[70%] sm:w-[60%] inf2_p'>最近因為想跟朋友再投資一間分店，需要預付房租訂金，感謝易速貸，讓我這麼快可以拿到資金</p>
-                </div>
+                </div> -->
                 <!-- 5 -->
-                <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
+                <div class='w-[50%] md:w-[33.3%] sm:w-full mix-[320px]:w-full flex flex-col items-center my-[40px]'>
+                    <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
+                        <img src='./asset/img/worker.png' class='w-[100px] h-auto'>
+                    </div>
+                    <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>雲林李先生<span class='ml-1 text-sm text-[#4d4d4d] font-semibold mb-10'>工頭</span></div>
+                    <p class='md:w-[80%] sm:w-[60%] min-[320px]:w-[60%] inf2_p'>工地都是領現金，沒存到什麼錢，最近出了場車禍，需要修車費，臨時拿不出來這麼多現金，還好有易速貸，量身幫我規劃，不然真的不知道怎麼辦</p>
+                </div>
+                <!-- <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
                     <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
                         <img src='./asset/img/worker.png' class='w-[100px] h-auto'>
                     </div>
                     <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>雲林李先生<span class='ml-1 text-sm text-[#4d4d4d] font-semibold mb-10'>工頭</span></div>
                     <p class='md:w-[70%] sm:w-[60%] inf2_p'>工地都是領現金，沒存到什麼錢，最近出了場車禍，需要修車費，臨時拿不出來這麼多現金，還好有易速貸，量身幫我規劃，不然真的不知道怎麼辦</p>
-                </div>
+                </div> -->
                 <!-- 6 -->
-                <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
+                <div class='w-[50%] md:w-[33.3%] sm:w-full mix-[320px]:w-full flex flex-col items-center my-[40px]'>
+                    <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
+                        <img src='./asset/img/tax-inspector.png' class='w-[100px] h-auto'>
+                    </div>
+                    <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>彰化張先生<span class='ml-1 text-sm text-[#4d4d4d] font-semibold mb-10'>退休</span></div>
+                    <p class='md:w-[80%] sm:w-[60%] min-[320px]:w-[60%] inf2_p'>年輕就退休的張先生，因為股票投資失利，但又不想跟親友借錢，後來看到易速貸，與專人聯繫，成功舒緩短期資金的壓力</p>
+                </div>
+                <!-- <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
                     <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
                         <img src='./asset/img/tax-inspector.png' class='w-[100px] h-auto'>
                     </div>
                     <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>彰化張先生<span class='ml-1 text-sm text-[#4d4d4d] font-semibold mb-10'>退休</span></div>
                     <p class='inf2_p'>年輕就退休的張先生，因為股票投資失利，但又不想跟親友借錢，後來看到易速貸，與專人聯繫，成功舒緩短期資金的壓力</p>
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
@@ -187,7 +223,7 @@
                 <div class='w-full md:w-[45%] flex flex-col md:flex-row mb-[15px]'>
                     <label class='w-[150px] text-xl'>聯絡時間 *</label>
                     <select name='sex' class='w-full md:w-[calc(100%-150px)] py-[6px] px-[7px] bg-white'>
-                        <option value='現在方便連絡'>現在方便連絡</option>
+                        <option value='0'>現在方便連絡</option>
                         <option value='9:00~10:00'>9:00~10:00</option>
                         <option value='10:00~11:00'>10:00~11:00</option>
                         <option value='11:00~12:00'>11:00~12:00</option>
@@ -269,51 +305,88 @@
         <div class='w-[95%] max-w-[1080px]' >
             <div class='title w-full flex justify-center mb-10'>服務項目</div>
             <div class='w-[95%] flex flex-wrap mx-auto'>
-                <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
+                <!-- 1 -->
+                <div class='w-[50%] md:w-[33.3%] sm:w-full mix-[320px]:w-full flex flex-col items-center my-[40px]'>
+                    <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
+                        <img src='./asset/img/5.PNG' class='w-[100px] h-auto'>
+                    </div>
+                    <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>身分證借款</div>
+                    <p class='md:w-[80%] sm:w-[60%] min-[320px]:w-[60%] inf2_p'>借款無需限定職業類別，快速放款能即時解決您的資金燃眉之急。我們提供寬鬆的放款條件和絕對保密的安全保障，無論您的信用紀錄是否有瑕疵、協商或遲繳都可以申請。立即向我們諮詢，我們將為您度身打造最適合您的借款方案。</p>
+                </div>
+                <!-- <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
                     <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
                         <img src='./asset/img/5.PNG' class='w-[100px] h-auto'>
                     </div>
                     <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>身分證借款</div>
                     <p class='inf2_p'>借款無需限定職業類別，快速放款能即時解決您的資金燃眉之急。我們提供寬鬆的放款條件和絕對保密的安全保障，無論您的信用紀錄是否有瑕疵、協商或遲繳都可以申請。立即向我們諮詢，我們將為您度身打造最適合您的借款方案。</p>
+                </div> -->
+                <!-- 2 -->
+                <div class='w-[50%] md:w-[33.3%] sm:w-full mix-[320px]:w-full flex flex-col items-center my-[40px]'>
+                    <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
+                        <img src='./asset/img/6.PNG' class='w-[100px] h-auto'>
+                    </div>
+                    <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>小額借款</div>
+                    <p class='md:w-[80%] sm:w-[60%] min-[320px]:w-[60%] inf2_p'>我們提供小額借款，無論您的職業類別是什麼，都可以申請。我們快速放款，讓您即時緩解燃眉之急。申請只需30分鐘，快速審核放款，也可選擇身分證借款等多種貸款服務，給您更多的選擇。我們的貸款服務能讓您安心借款，讓您更有信心面對未來。</p>
                 </div>
-                <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
+                <!-- <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
                     <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
                         <img src='./asset/img/6.PNG' class='w-[100px] h-auto'>
                     </div>
                     <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>小額借款</div>
                     <p class='inf2_p'>我們提供小額借款，無論您的職業類別是什麼，都可以申請。我們快速放款，讓您即時緩解燃眉之急。申請只需30分鐘，快速審核放款，也可選擇身分證借款等多種貸款服務，給您更多的選擇。我們的貸款服務能讓您安心借款，讓您更有信心面對未來。</p>
-                </div>
+                </div> -->
                 <!-- 3 -->
-                <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
+                <div class='w-[50%] md:w-[33.3%] sm:w-full mix-[320px]:w-full flex flex-col items-center my-[40px]'>
+                    <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
+                        <img src='./asset/img/7.PNG' class='w-[100px] h-auto'>
+                    </div>
+                    <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>商家週轉金</div>
+                    <p class='md:w-[80%] sm:w-[60%] min-[320px]:w-[60%] inf2_p'>商家運營時常遇到資金短缺的情況，這時準備好的週轉金就能夠協助您渡過難關。由於這種情況通常十分緊急，我們提供輕鬆貸款，讓您能夠快速取得所需資金，無需擔心因緊急情況而無法及時籌措資金。</p>
+                </div>
+                <!-- <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
                     <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
                         <img src='./asset/img/7.PNG' class='w-[100px] h-auto'>
                     </div>
                     <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>商家週轉金</div>
                     <p class='inf2_p'>商家運營時常遇到資金短缺的情況，這時準備好的週轉金就能夠協助您渡過難關。由於這種情況通常十分緊急，我們提供輕鬆貸款，讓您能夠快速取得所需資金，無需擔心因緊急情況而無法及時籌措資金。</p>
-                </div>
+                </div> -->
                 <!-- 4 -->
-                <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
+                <div class='w-[50%] md:w-[33.3%] sm:w-full mix-[320px]:w-full flex flex-col items-center my-[40px]'>
+                    <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
+                        <img src='./asset/img/8.PNG' class='w-[100px] h-auto'>
+                    </div>
+                    <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>疫情紓困金</div>
+                    <p class='md:w-[80%] sm:w-[60%] min-[320px]:w-[60%] inf2_p'>為因應疫情帶來的影響，我們特別針對受疫情衝擊的勞工、產業和企業提供疫情紓困金。我們的紓困計劃包含「發放現金、提供貸款、減輕負擔」三個方面，擴大了紓困對象，簡化了申請流程。我們的勞工紓困貸款不受勞保限制，讓更多的人能夠受惠。</p>
+                </div>
+                <!-- <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
                     <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
                         <img src='./asset/img/8.PNG' class='w-[100px] h-auto'>
                     </div>
                     <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>疫情紓困金</div>
                     <p class='inf2_p'>為因應疫情帶來的影響，我們特別針對受疫情衝擊的勞工、產業和企業提供疫情紓困金。我們的紓困計劃包含「發放現金、提供貸款、減輕負擔」三個方面，擴大了紓困對象，簡化了申請流程。我們的勞工紓困貸款不受勞保限制，讓更多的人能夠受惠。</p>
-                </div>
+                </div> -->
                 <!-- 5 -->
-                <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
+                <div class='w-[50%] md:w-[33.3%] sm:w-full mix-[320px]:w-full flex flex-col items-center my-[40px]'>
+                    <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
+                        <img src='./asset/img/9.PNG' class='w-[100px] h-auto'>
+                    </div>
+                    <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>支票借款</div>
+                    <p class='md:w-[80%] sm:w-[60%] min-[320px]:w-[60%] inf2_p'>我們提供支票借款服務，讓您能夠快速地將支票轉換為現金，無論您是個人戶還是公司戶，無論支票期限長短，我們都能全額貼現，避免您需要繁瑣的銀行手續。我們還能代替錢莊和當舖提供貼現服務，讓您在資金運用上更加省心省力。</p>
+                </div>
+                <!-- <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
                     <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
                         <img src='./asset/img/9.PNG' class='w-[100px] h-auto'>
                     </div>
                     <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>支票借款</div>
                     <p class='inf2_p'>我們提供支票借款服務，讓您能夠快速地將支票轉換為現金，無論您是個人戶還是公司戶，無論支票期限長短，我們都能全額貼現，避免您需要繁瑣的銀行手續。我們還能代替錢莊和當舖提供貼現服務，讓您在資金運用上更加省心省力。</p>
-                </div>
+                </div> -->
                 <!-- 6 -->
-                <div class='w-[50%] md:w-[33.3%] sm:w-full min-[320px]:w-full flex flex-col items-center my-[40px]'>
+                <div class='w-[50%] md:w-[33.3%] sm:w-full mix-[320px]:w-full flex flex-col items-center my-[40px]'>
                     <div class='w-[100px] h-[100px] flex justify-center items-center mb-5'>
                         <img src='./asset/img/10.PNG' class='w-[100px] h-auto'>
                     </div>
                     <div class='text-xl text-[#4d4d4d] font-semibold mb-10'>民間借款整合</div>
-                    <p class='inf2_p'>我們提供民間借款整合服務，無論您的個人條件如何，我們都能免徵信和免保人，快速放款並且沒有區域限制。我們的民間貸款服務讓您靈活取得資金，申辦流程簡便，且核貸速度快，避免了銀行高門檻和錢莊高利息的問題。</p>
+                    <p class='md:w-[80%] sm:w-[60%] min-[320px]:w-[60%] inf2_p'>我們提供民間借款整合服務，無論您的個人條件如何，我們都能免徵信和免保人，快速放款並且沒有區域限制。我們的民間貸款服務讓您靈活取得資金，申辦流程簡便，且核貸速度快，避免了銀行高門檻和錢莊高利息的問題。</p>
                 </div>
             </div>
         </div>
